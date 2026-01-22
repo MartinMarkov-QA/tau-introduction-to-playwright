7b2020ffee2e19b24472a8b9f29ed5033aa6ab6f
Martin Markov <martin.markov1980@gmail.com>
2026-01-22 17:34:32 +0000

    Example test

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   retries: 2,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.only('has title', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -31,20 +31,16 @@ test('get started link', async ({ page }) => {
  * 
  */
 
-// test.only('check Java page', async ({ page }) => {
-test('check Java page', async ({ page }) => {
+test.describe('@UI Navigate and test Node and Java pages titles', () => {
+  test('Verify Java and Node page titles', async ({ page, context }) => {
 
-  await page.goto('https://playwright.dev');
-
-  await page.getByRole('link', {name: 'Get started'}).click();
-  await page.getByRole('button', {name: 'Node.js'}).hover();
-  await page.getByText('Java', { exact:true }).click();
-  // await page.getByRole('navigation', { name: 'Main' }).getByText('Java').click(); // in case the locator above doesn't work, you can use this line. Remove the line above and use this one instead.
-
-  await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
-  await expect(page.getByText('Installing Playwright', {exact:true})).not.toBeVisible();
-
-  const javaDescription = `Playwright is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project's pom.xml as described below. If you're not familiar with Maven please refer to its documentation.`;
-  await expect(page.getByText(javaDescription)).toBeVisible();
-
-});
+      await test.step('Verify Java page title', async () => {
+      
+        await page.goto('https://playwright.dev/');
+        expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright');
+        await page.getByRole('button', { name: 'Node.js' }).hover();
+        await page.locator('a').filter({ hasText: 'Java' }).first().click();
+        expect(page).toHaveURL('https://playwright.dev/java/');
+      });
+    });
+  });
