4880c268183185c60e4546296e8d8b5e614a32df
Martin Markov <martin.markov1980@gmail.com>
2026-02-07 13:33:29 +0000

    Added new test and config changes.

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,8 +20,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
 
   /* Retry on CI only */
-  // retries: process.env.CI ? 2 : 0,
-  retries: 2,
+  retries: process.env.CI ? 2 : 1,
 
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
@@ -47,8 +46,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /* Collect trace for every test run. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-all-retries',
     screenshot: 'only-on-failure',
     // headless: false,
     // ignoreHTTPSErrors: true,
--- tests/practice.spec.ts
+++ tests/practice.spec.ts
@@ -4,10 +4,11 @@ test.describe("Your test suite name", () => {
     test("Verify Playwright homepage title", async ({ page }) => {
         await test.step("Go to Playwright web site", async () => {
             await page.goto("https://playwright.dev/");
+            await page.getByRole('link', { name: 'Docs' }).click();
         });
-        test.step("Verify the page title", async () => {
+        await test.step("Verify the page title", async () => {
             await expect(page).toHaveTitle(
-                "Fast and reliable end-to-end testing for modern web apps | Playwright",
+                "Installation | Playwright",
             );
         });
     });
